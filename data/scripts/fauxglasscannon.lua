5b2f86d869eead703534098e286ebf9fa69e5978
François Gombault <francois.gombault@posteo.de>
2023-08-07 16:43:36 +0200

    fixed hard game crash when essence of earth was shooting
    ComponentObjectSetValue is deprecated

--- data/scripts/fauxglasscannon.lua
+++ data/scripts/fauxglasscannon.lua
@@ -24,17 +24,24 @@ function shot( entity_id )
 			ComponentSetValue2( comp, "damage", damage )
 			
 			local dtypes = { "projectile", "explosion", "melee", "ice", "slice", "electricity", "radioactive", "drill", "fire" }
-			for a,b in ipairs(dtypes) do
-				local v = tonumber(ComponentObjectGetValue( comp, "damage_by_type", b ))
-				v = v * damagemodifier
-				ComponentObjectSetValue( comp, "damage_by_type", b, tostring(v) )
+			local damage_by_types = ComponentObjectGetMembers(projectile, "damage_by_type") or {};
+			for i, damageByTypeComp in ipairs(damage_by_types) do
+				for a, b in ipairs(dtypes) do
+					local v = ComponentGetValue2(damageByTypeComp, b)
+					v = v * damagemodifier
+					ComponentSetValue2(damageByTypeComp, b, v)
+				end
 			end
-			
-			local etypes = { "explosion_radius", "ray_energy", "sparks_count_min", "sparks_count_max", "camera_shake", "damage", "material_sparks_count_min", "material_sparks_count_max", "stains_radius" }
-			for a,b in ipairs(etypes) do
-				local v = tonumber(ComponentObjectGetValue2( comp, "config_explosion", b ))
-				v = v * explosionmodifier
-				ComponentObjectSetValue( comp, "config_explosion", b, tostring(v) )
+
+			local etypes = { "explosion_radius", "ray_energy", "sparks_count_min", "sparks_count_max", "camera_shake",
+				"damage", "material_sparks_count_min", "material_sparks_count_max", "stains_radius" }
+			local explosionConfigs = ComponentObjectGetMembers(projectile, "config_explosion") or {};
+			for i, explosionConfigComp in ipairs(explosionConfigs) do
+				for a, b in ipairs(etypes) do
+					local v = ComponentGetValue2(explosionConfigComp, b)
+					v = v * damagemodifier
+					ComponentSetValue2(explosionConfigComp, b, v)
+				end
 			end
 		end
 	end
