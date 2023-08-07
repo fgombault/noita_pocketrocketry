60b4a435c188c051ee15882a62ffb20594cbb852
François Gombault <francois.gombault@posteo.de>
2023-08-07 17:34:54 +0200

    also modify projectile lifetime

--- config.lua
+++ config.lua
@@ -1,4 +1,16 @@
+-- set this to 1 if you want wand damage to be affected by the explosion size multiplier.
+-- set this to -1 if you want wand damage to be inversely affected by it
+-- modify_damage_values_too = 1
+-- modify_lifetime_values_too = 1
 
---set this to true if you want wand damage to be affected by the explosion size multiplier.
+-- example: only modify explosion radius, but not damage or lifetime
+-- modify_damage_values_too = 0
+-- modify_lifetime_values_too = 0
 
-modify_damage_values_too = false
+-- example: when radius and damage are increased, lifetime is reduced. dangerous! funny!
+modify_damage_values_too = 1
+modify_lifetime_values_too = -1
+
+-- example: only modifies radius and damage, leave lifetime out of it
+-- modify_damage_values_too = 1
+-- modify_lifetime_values_too = 0
--- data/scripts/fauxglasscannon.lua
+++ data/scripts/fauxglasscannon.lua
@@ -6,19 +6,33 @@ dofile_once("mods/pocketrocketry/config.lua")
 function shot(entity_id)
 	local explosionmodifier = getCurrentlyEquippedWandKaboom()
 	local damagemodifier = 1
-	if modify_damage_values_too == 1 then
+	if (modify_damage_values_too > 0) then
 		damagemodifier = explosionmodifier
+	elseif (modify_damage_values_too < 0) then
+		damagemodifier = 1 / explosionmodifier
 	end
-	
-	--print("shot explosion modifier:"..explosionmodifier)
+	local lifetimemodifier = 1
+	if (modify_lifetime_values_too > 0) then
+		lifetimemodifier = explosionmodifier
+	elseif (modify_lifetime_values_too < 0) then
+		lifetimemodifier = 1 / explosionmodifier
+	end
 
-	local comps = EntityGetComponent( entity_id, "ProjectileComponent" )
-	if( comps ~= nil ) then
-		for i,comp in ipairs(comps) do
-			local damage = ComponentGetValue2( comp, "damage" )
+	-- print("shot explosion modifier:" .. explosionmodifier)
+
+	local comps = EntityGetComponent(entity_id, "ProjectileComponent")
+	if (comps ~= nil) then
+		for _, projectile in ipairs(comps) do
+			local damage = ComponentGetValue2(projectile, "damage")
 			damage = damage * damagemodifier
-			ComponentSetValue2( comp, "damage", damage )
-			
+			ComponentSetValue2(projectile, "damage", damage)
+
+			local lifetime = ComponentGetValue2(projectile, "lifetime")
+			if (lifetime ~= -1) then
+				lifetime = math.floor(lifetime * lifetimemodifier)
+				ComponentSetValue2(projectile, "lifetime", lifetime)
+			end
+
 			local dtypes = { "projectile", "explosion", "melee", "ice", "slice", "electricity", "radioactive", "drill", "fire" }
 			local damage_by_types = ComponentObjectGetMembers(projectile, "damage_by_type") or {};
 			for i, damageByTypeComp in ipairs(damage_by_types) do
