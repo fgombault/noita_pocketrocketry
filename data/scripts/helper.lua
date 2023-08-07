2f6ce8c10c5d511ab79544f64d8a51e9ae82e126
François Gombault <francois.gombault@posteo.de>
2023-08-07 17:28:27 +0200

    formatting

--- data/scripts/helper.lua
+++ data/scripts/helper.lua
@@ -64,25 +64,25 @@ function getCurrentlyEquippedWandKaboom()
 end
 
 function addNewInternalVariable(entity_id, variable_name, variable_type, initial_value)
-    if(variable_type == "value_int") then
+    if (variable_type == "value_int") then
         EntityAddComponent2(entity_id, "VariableStorageComponent", {
-            name=variable_name,
-            value_int=initial_value
+            name = variable_name,
+            value_int = initial_value
         })
-    elseif(variable_type == "value_string") then
+    elseif (variable_type == "value_string") then
         EntityAddComponent2(entity_id, "VariableStorageComponent", {
-            name=variable_name,
-            value_string=initial_value
+            name = variable_name,
+            value_string = initial_value
         })
-    elseif(variable_type == "value_float") then
+    elseif (variable_type == "value_float") then
         EntityAddComponent2(entity_id, "VariableStorageComponent", {
-            name=variable_name,
-            value_float=initial_value
+            name = variable_name,
+            value_float = initial_value
         })
-    elseif(variable_type == "value_bool") then
+    elseif (variable_type == "value_bool") then
         EntityAddComponent2(entity_id, "VariableStorageComponent", {
-            name=variable_name,
-            value_bool=initial_value
+            name = variable_name,
+            value_bool = initial_value
         })
     end
 end
@@ -102,13 +102,12 @@ function getInternalVariableValue(entity_id, variable_name, variable_type)
 end
 
 function setInternalVariableValue(entity_id, variable_name, variable_type, new_value)
-
-    local components = EntityGetComponent( entity_id, "VariableStorageComponent" )    
-    if ( components ~= nil ) then
-        for key,comp_id in pairs(components) do 
-            local var_name = ComponentGetValue2( comp_id, "name" )
-            if( var_name == variable_name) then
-                ComponentSetValue2( comp_id, variable_type, new_value )
+    local components = EntityGetComponent(entity_id, "VariableStorageComponent")
+    if (components ~= nil) then
+        for key, comp_id in pairs(components) do
+            local var_name = ComponentGetValue2(comp_id, "name")
+            if (var_name == variable_name) then
+                ComponentSetValue2(comp_id, variable_type, new_value)
             end
         end
     end
